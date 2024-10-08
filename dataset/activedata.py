d84e976f9ca10923e5008dfe7f7518c5cd02ced9
Yunpyo An <anyunpyo@unist.ac.kr>
2024-10-08 22:00:57 +0900

    update type

--- dataset/activedata.py
+++ dataset/activedata.py
@@ -2,8 +2,10 @@ from typing import (
     Callable,
     List,
     Optional,
+    
 )
 
+import numpy as np
 import torch
 from torch.utils.data import Dataset
 
@@ -11,15 +13,23 @@ from torch.utils.data import Dataset
 # Active learning dataset abstraction
 class ActiveLearningDataset(Dataset):
     def __init__(self,
-                 samples: List[torch.Tensor],
-                 targets: Optional[List[torch.Tensor]] = None,
-                 transform: Optional[Callable] = None,):
+                 samples: List[torch.Tensor | np.ndarray],
+                 targets: Optional[List[torch.Tensor | np.ndarray]] = None,
+                 transform: Optional[Callable] = None,
+                 dataloader_config: Optional[dict] = None):
         r"""Dataset for active learning.
         
         """
+        if isinstance(samples, np.ndarray):
+            samples = torch.from_numpy(samples).float()
         self.samples = samples
+    
+        if isinstance(targets, list):
+            targets = torch.tensor(targets)
         self.targets = targets
+    
         self.transform = transform
+        self.dataloader_config = dataloader_config
         
         self.labeled_indices = []
     
@@ -40,8 +50,14 @@ class ActiveLearningDataset(Dataset):
     def _label_data(self, idx, label):
         self.targets[idx] = label
     
-    def add_label(self, idx):
-        self.labeled_indices.append(idx)
+    def add_label(self,
+                  idx: int | List[int],):
+        if isinstance(idx, int):
+            idx = [idx]
+        self.labeled_indices.extend(idx)
     
     def get_unlabeled_index(self):
-        return list(set(range(len(self.samples))) - set(self.labeled_indices))[0]
+        return list(set(range(len(self.samples))) - set(self.labeled_indices))
+    
+    def get_dataloader(self):
+        return torch.utils.data.DataLoader(self, **self.dataloader_config)
