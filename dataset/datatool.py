df61f573f0bddd01ff762bc924737d6107f97972
Yunpyo An <anyunpyo@unist.ac.kr>
2024-10-08 21:17:09 +0900

    change loader

--- dataset/datatool.py
+++ dataset/datatool.py
@@ -14,28 +14,21 @@ def get_dataset(config):
     
     assert dataset_name in data_dict, f"Dataset {dataset_name} not found"
     
+    loader_config = config['DATASET']['dataloader']
+    
     train_dataset = data_dict[dataset_name](root=config['DATASET']['root'], 
                                             train=True, download=True)
     test_dataset = data_dict[dataset_name](root=config['DATASET']['root'],
                                              train=False, download=True)
     
-    train_active_dataset = ActiveLearningDataset(samples=train_dataset.data,
-                                                    targets=train_dataset.targets)
-    test_active_dataset = ActiveLearningDataset(samples=test_dataset.data,
-                                                targets=test_dataset.targets)
-    
-    return train_active_dataset, test_active_dataset
-
-
-def get_dataloader(train_dataset, test_dataset, config):
-    loader_config = config['DATASET']['dataloader']
-    
     loader_config['shuffle'] = True
-    train_loader = torch.utils.data.DataLoader(train_dataset,
-                                                **loader_config)
+    train_active_dataset = ActiveLearningDataset(samples=train_dataset.data,
+                                                 targets=train_dataset.targets,
+                                                 dataloader_config=loader_config)
     
     loader_config['shuffle'] = False
-    test_loader = torch.utils.data.DataLoader(test_dataset,
-                                                **loader_config)
+    test_active_dataset = ActiveLearningDataset(samples=test_dataset.data,
+                                                targets=test_dataset.targets,
+                                                dataloader_config=loader_config)
     
-    return train_loader, test_loader
+    return train_active_dataset, test_active_dataset
