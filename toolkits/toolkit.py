5748afce366b31f2674c3290fa083fa918c350f2
Yunpyo An <anyunpyo@unist.ac.kr>
2024-10-08 20:34:16 +0900

    load yaml config

--- toolkits/toolkit.py
+++ toolkits/toolkit.py
@@ -1,8 +1,17 @@
 import os
 
+import yaml
+
 def is_path(path):
     # reference: https://stackoverflow.com/questions/38834378/path-to-a-directory-as-argparse-argument
     if os.path.isdir(path):
         return path
     else:
         raise NotADirectoryError(f"{path} is not a directory")
+    
+
+def load_config(config_path):
+    with open(config_path, "r") as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+    
+    return config
